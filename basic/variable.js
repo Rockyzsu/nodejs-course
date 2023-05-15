d1c0afe7d023cb38aceab77d8f249d535cdb995e
Rockyzsu <jinweizsu@gmail.com>
2023-05-15 10:19:10 +0800

    first commit

---
+++ basic/variable.js
@@ -0,0 +1,22 @@
+var a =1;
+var a =2;
+
+let b=1;
+// let b =2; wrong
+//
+
+console.log(c);
+var c = 10;
+
+
+//console.log(d); wroing
+//let d =1;
+
+
+var fn = v=>console.log(v)
+fn('aa');
+
+
+console.log('end of js');
+
+
