6b316b553c14253f81feab9c2e8a0d3e0af62d2b
Rocky Chen <jinweizsu@gmail.com>
2023-07-25 11:35:39 +0800

    update

--- basic/this-usage.js
+++ basic/this-usage.js
@@ -11,10 +11,30 @@ console.log(person.name)
 person.greet()
 
 
-function thisFunc(){return this}
+function thisFunc() {
+    return this
+}
 
-var testObj  = {
-  func:thisFunc
+var testObj = {
+    func: thisFunc
 }
 
 console.log(testObj.func())
+
+function globalAlias() {
+    return this;
+}
+
+console.log(globalAlias())
+
+
+var f = {
+    name: 'fork',
+    func: function() {
+        return this.name
+    }
+    func1: function() {
+        return this
+    }
+}
+console.log(f.func())
