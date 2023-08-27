0d421101cdaecf41289fd4f75823e723e92f6f1d
Rocky Chen <jinweizsu@gmail.com>
2023-08-27 19:35:50 +0800

    'order'

---
+++ basic/function_order.js
@@ -0,0 +1,20 @@
+// 定义顺序
+
+let port = normalizePort('1024')
+console.log(port)
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
