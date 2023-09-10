6f92c2c08c032dc37774cbb5d2c2c51c93579aec
Rocky Chen <jinweizsu@gmail.com>
2023-09-10 23:56:53 +0800

    change exec

--- project/jandan-net/src/debug_find.js
+++ project/jandan-net/src/debug_find.js
@@ -8,7 +8,7 @@ const { janDanModel, createDoc, janDanStop } = require("./models.js");
 async function findOne() {
   ret = await janDanModel
     .findOne({ comment_id: "5538618" }, "comment_id")
-    // .exec();
+    .exec();
   console.log("is exist return ", typeof ret);
   console.log(ret);
   janDanStop()
