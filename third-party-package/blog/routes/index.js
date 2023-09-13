882e9fdba07c3ebb74eef394e36669bc2c05c39c
Rocky Chen <jinweizsu@gmail.com>
2023-09-13 19:21:48 +0800

    just test in server

--- third-party-package/blog/routes/index.js
+++ third-party-package/blog/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.json({ status: 200, name: "joe" });
+});
+
+/* GET home page. */
+router.get("/api", function (req, res, next) {
+  res.render({ version: 1 });
 });
 
 module.exports = router;
