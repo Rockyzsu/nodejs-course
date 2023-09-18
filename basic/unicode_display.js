0b60198b4f5ec352bf3805fc34ba63332f6cc926
Rocky Chen <jinweizsu@gmail.com>
2023-09-18 11:56:19 +0800

    book sample

---
+++ basic/unicode_display.js
@@ -0,0 +1,8 @@
+const string = "This page \u00A9 Shelley Powers";
+console.log(string);
+
+const hamburgeryStore = "Hamburgery\u2122";
+console.log(hamburgeryStore);
+
+const hamburgeryStore2 = "Hamburgery " + String.fromCodePoint(0x1F354);
+console.log(hamburgeryStore2);
