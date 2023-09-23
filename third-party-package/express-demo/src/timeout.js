d626edfde4b3e7dbdc05ccc55e75652bec626523
Rocky Chen <jinweizsu@gmail.com>
2023-09-24 00:27:29 +0800

    update

---
+++ third-party-package/express-demo/src/timeout.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 7000
+
+app.use(express.json()); // 允许处理json
+
+app.get('/',(req,res)=>{
+    console.log(new Date())
+    setTimeout(()=>{
+        res.send("OK")
+    },2000)
+    console.log(new Date())
+
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    })
