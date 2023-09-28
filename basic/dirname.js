0da78d806e89dde92bfea197e521fe837e7515b1
Rocky Chen <jinweizsu@gmail.com>
2023-09-28 08:47:27 +0800

    dir

--- basic/dirname.js
+++ basic/dirname.js
@@ -1,2 +1,13 @@
 const current_path = __dirname;
 console.log(current_path);
+var path = require("path");
+var fs = require("fs");
+const full_path = path.resolve("temp", "data.txt");
+fs.readFile(full_path, (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.toString());
+});
+
