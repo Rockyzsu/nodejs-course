b326981d58482eeaa4d8b332a97c1960f4564cd5
Rockyzsu <jinweizsu@gmail.com>
2023-12-18 00:23:03 +0800

    update

--- third-party-package/sequelize-mysql-orm/app.js
+++ third-party-package/sequelize-mysql-orm/app.js
@@ -1,29 +1,45 @@
 var WishModel = require("./wish");
 task = {
-    query: async () => {
-        WishModel.findAll({ limit: 10 })
-            .then(function (result) {
-                let list = [];
-                result.forEach((v, i) => {
-                    let obj = { id: v.id, name: v.name, content: v.content };
-                    list.push(obj);
-                });
-                console.log(list);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    },
-    create: async () => {
-        for (let i = 0; i < 10; i++) {
-            WishModel.create({ name: `name ${i}`, content: `content: ${i}` })
-                .then((result) => {
-                    console.log(result);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-    },
+  query: async () => {
+    WishModel.findAll({ limit: 10 })
+      .then(function (result) {
+        let list = [];
+        result.forEach((v, i) => {
+          let obj = { id: v.id, name: v.name, content: v.content };
+          list.push(obj);
+        });
+        console.log(list);
+      })
+      .then(() => {
+        console.log(" ========================== query ========================");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  },
+  create: async () => {
+    for (let i = 0; i < 10; i++) {
+      WishModel.create({ name: `name ${i}`, content: `content: ${i}` })
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  },
+  init: async () => {
+    const result = await WishModel.sync({ force: true }).then(() => {
+      console.log("table has been syncd");
+      return "godaddy";
+    });
+    console.log(result);
+  },
 };
-task.query();
+async function main() {
+  // await task.init();
+  // await task.create();
+  await task.query();
+}
+main();
+console.log("end");
--- third-party-package/sequelize-mysql-orm/wish.js
+++ third-party-package/sequelize-mysql-orm/wish.js
@@ -1,20 +1,21 @@
 // 定义表结构
-const db = require("./db");
+const sequelize = require("./db");
+// const db = require("./db");
 var Sequelize = require("sequelize");
 
-const Wish = db.define(
-    "Wish",
-    {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true,
-        },
-        name: { type: Sequelize.STRING(20), allowNull: false },
-        content: { type: Sequelize.STRING, allowNull: false },
+const Wish = sequelize.define(
+  "Wish",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
     },
-    { underscored: true, tableName: "wish" }
+    name: { type: Sequelize.STRING(20), allowNull: false },
+    content: { type: Sequelize.STRING, allowNull: false },
+  },
+  { underscored: true, tableName: "wish" }
 );
 
 module.exports = Wish;
