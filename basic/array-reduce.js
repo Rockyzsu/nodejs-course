41f5de07feb01f10f26f102487e476c56570573c
Rocky Chen <jinweizsu@gmail.com>
2024-03-27 18:16:44 +0800

    reduce

--- basic/array-reduce.js
+++ basic/array-reduce.js
@@ -11,3 +11,39 @@ const getTotal = () => {
 
 getTotal();
 // console.log(value);
+
+// 联系题目
+let name_arr = ["Joe", "Jack", "William", "Averell"];
+let name_result = name_arr.reduce((result, start_value) => {
+  return result + start_value + ",";
+}, "");
+
+console.log(name_result); // Joe,Jack,William,Averell,
+
+const age_obj = [
+  { name: "joe", age: 19 },
+  { name: "young", age: 73 },
+  { name: "shock", age: 23 },
+];
+
+total_age = age_obj.reduce((result, start) => {
+  return result + start.age;
+}, 0);
+
+console.log(total_age); // 115
+
+const age_obj2 = [
+  { name: "joe", age: 19 },
+  { name: "young", age: 73 },
+  { name: "shock", age: 23 },
+];
+
+total_age = age_obj2.reduce((result, start) => {
+  if (start.age > 50) {
+    return result + start.age;
+  } else {
+    return result;
+  }
+}, 0);
+
+console.log(total_age); // 73
