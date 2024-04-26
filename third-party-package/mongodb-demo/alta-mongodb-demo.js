c11856b59dd68153f39194e2edb7f3de34f53f02
Rocky Chen <jinweizsu@gmail.com>
2024-04-26 21:34:57 +0800

    updated

--- third-party-package/mongodb-demo/alta-mongodb-demo.js
+++ third-party-package/mongodb-demo/alta-mongodb-demo.js
@@ -19,7 +19,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    await client.db('db_parker').collection('users').insertOne({name: 'John Doe'});
+    await client.db('db_parker').collection('users').insertOne({name: 'John Doe',updated: new Date()});
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
