922af11641d631c9b6c6ab08c4a9b8e52be3204b
Rocky Chen <jinweizsu@gmail.com>
2024-10-19 01:32:32 +0800

    update

---
+++ HTML/mine_game.js
@@ -0,0 +1,11 @@
+function main() {
+    var mineMap = []
+    for (let i = 0; i < 100; i++) {
+        mineMap[mineMap.length] = {
+            mine: 0
+        }
+    }
+    console.log(mineMap)
+}
+
+main()
